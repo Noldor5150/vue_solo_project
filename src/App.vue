3564b0d5a1a6206ea0fa8e6c15952f3628974605
Noldor5150 <ruikis.paulius@gmail.com>
2019-10-14 18:57:59 +0300

    basic components, login registration validation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
 <template>
-  <h1>alio</h1>
+  <v-app>
+    <v-navigation-drawer app temporary v-model="drawer" color="primary" dark>
+      <v-list>
+        <v-list-item v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-item-icon>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-card>
+      <v-toolbar dense dark class="deep-purple accent-3">
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+        <v-toolbar-title>Add Project</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn
+            class="deep-purple accent-3"
+            text
+            v-for="link of links"
+            :key="link.title"
+            :to="link.url"
+          >
+            <v-icon left>{{link.icon}}</v-icon>
+            {{link.title}}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-card>
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Sign-Up", icon: "mdi-face", url: "/registration" },
+        { title: "Orders", icon: "mdi-bookmark", url: "/orders" },
+        { title: "New Post", icon: "mdi-post", url: "/new" },
+        { title: "My Posts", icon: "mdi-view-list", url: "/list" }
+      ]
+    };
+  }
+};
 </script>
+<style lang="sass">
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,53 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from '@/components/Home';
+import Login from './components/Auth/Login.vue';
+import Registration from './components/Auth/Registration.vue';
+import NewPost from './components/Posts/NewPost.vue';
+import Post from './components/Posts/Post.vue';
+import PostList from './components/Posts/PostList.vue';
+import Orders from './components/User/Orders.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [],
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/post/:id',
+      name: 'post',
+      component: Post,
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: PostList,
+    },
+    {
+      path: '/new',
+      name: 'newPost',
+      component: NewPost,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: Registration,
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders,
+    },
+  ],
 });
